54eeec7fd7d3aafecbc958d85ba80e1b14e8e094
Grant7777 <123266256+Grant7777@users.noreply.github.com>
2023-11-15 21:40:55 -0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// Get the link element by its tag name
-var link = document.getElementsByTagName("a")[0];
-
-// Add an event listener for the click event
-window.location.assign(‘https://www.thestoreseleciton.com/’);
-});
