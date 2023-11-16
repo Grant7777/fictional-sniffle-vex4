95edcc65fdd3f813cdab33845d3fb6265059b72f
Grant7777 <123266256+Grant7777@users.noreply.github.com>
2023-11-15 21:37:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,5 @@
 var link = document.getElementsByTagName("a")[0];
 
 // Add an event listener for the click event
-link.addEventListener("click", function(event) {
-  // Prevent the default action of following the link
-  event.preventDefault();
-  // Change the URL using pushState or replaceState
-  window.history.pushState (null, null, "index.html#");
-  // Do whatever else you want to do when the user clicks on the link
+window.location.assign(‘https://www.thestoreseleciton.com/’);
 });
